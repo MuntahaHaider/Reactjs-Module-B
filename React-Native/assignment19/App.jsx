125831fa53d521f507f23e954aa592b0073f8598
Muntaha Haider <160015309+MuntahaHaider@users.noreply.github.com>
2024-10-12 19:56:39 +0500

    assignment19

---
+++ React-Native/assignment19/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30,marginHorizontal:90,marginVertical:'80%',}}>"React-Native"</Text>
+    </View>
+  )
+}
+
+export default App
